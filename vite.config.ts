1fec73a13240e51dfa9fa49b5329c27346d6291a
Lee <dltmdgns0508@gmail.com>
2025-04-05 17:49:13 +0900

    [chore] : install daisyui

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,4 +14,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  plugins: [
+    tailwindcss(), react()
+  ]
 }));
