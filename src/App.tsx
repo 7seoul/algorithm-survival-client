43a67e5f8f5b147f6df83bf54a45c5dc9c169a1d
Lee <dltmdgns0508@gmail.com>
2025-04-05 14:45:00 +0900

    update config setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <>
-      <h1 className='text-4xl text-blue-500'>hello</h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<></>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router'
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>
-)
+createRoot(document.getElementById('root')!).render(<App />)
