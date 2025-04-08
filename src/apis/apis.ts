7df2b4f3325cfa247e5788c21b3bac438052147a
gonudayo <kgw010119@gmail.com>
2025-04-08 10:34:32 +0900

    feat: set netlify proxy

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,27 +1,30 @@
-import { GroupRankResponse, GroupMainResponse, GroupInfoResponse, GroupListResponse, UserRankResponse } from "@/types/GroupType";
 import ky from "ky";
 
+const api = ky.create({
+  prefixUrl: import.meta.env.VITE_API_URL,
+  credentials: "include",
+});
 
-export const getGroupRank = async (type : 'score' | 'streak'):Promise<GroupRankResponse>=>{
-  return await ky.get(`${import.meta.env.VITE_API_URL}/rankings/groups/${type}`).json()
-}
+export const getGroupRank = async (type: "score" | "streak") => {
+  return await api.get(`rankings/groups/${type}`).json();
+};
 
-export const getUserRank = async (type : 'score' | 'streak'):Promise<UserRankResponse> => {
-  return await ky.get(`${import.meta.env.VITE_API_URL}/rankings/users/${type}`).json() 
-}
+export const getUserRank = async (type: "score" | "streak") => {
+  return await api.get(`rankings/users/${type}`).json();
+};
 
-export const getGroupMain = async ():Promise<GroupMainResponse> => {
-  return await ky.get(`${import.meta.env.VITE_API_URL}/rankings/groups/main`).json()
-}
+export const getGroupMain = async () => {
+  return await api.get(`rankings/groups/main`).json();
+};
 
-export const getGroupInfo = async (id:string) : Promise<GroupInfoResponse> => {
-  return await ky.get(`${import.meta.env.VITE_API_URL}/groups/${id}`).json()
-}
+export const getGroupInfo = async (id: string) => {
+  return await api.get(`groups/${id}`).json();
+};
 
-export const getAvatarUrl = (id:number) : string =>{
-  return `https://api.dicebear.com/9.x/thumbs/svg?seed=${id*10}&scale=80`
-}
+export const getGroupList = async () => {
+  return await api.get(`groups`).json();
+};
 
-export const getGroupList = async ():Promise<GroupListResponse> => {
-  return await ky.get(`${import.meta.env.VITE_API_URL}/groups`).json()
-}
+export const getAvatarUrl = (id: number): string => {
+  return `https://api.dicebear.com/9.x/thumbs/svg?seed=${id * 10}&scale=80`;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://34.64.193.149:8000/api/v2",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   resolve: {
     alias: {
