0a7df92aed6a62cdf0314fbda5127314bd02e0c0
gonudayo <kgw010119@gmail.com>
2025-04-08 10:58:13 +0900

    delete: env

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -1,27 +1,31 @@
 import { GroupRankResponse, GroupMainResponse, GroupInfoResponse, GroupListResponse, UserRankResponse } from "@/types/GroupType";
 import ky from "ky";
 
+const api = ky.create({
+  prefixUrl: "/api", 
+  credentials: "include", 
+});
 
 export const getGroupRank = async (type: 'score' | 'streak'): Promise<GroupRankResponse> => {
-  return await ky.get(`/api/rankings/groups/${type}`).json();
+  return await api.get(`rankings/groups/${type}`).json();
 }
 
 export const getUserRank = async (type: 'score' | 'streak'): Promise<UserRankResponse> => {
-  return await ky.get(`/api/rankings/users/${type}`).json();
+  return await api.get(`rankings/users/${type}`).json();
 }
 
 export const getGroupMain = async (): Promise<GroupMainResponse> => {
-  return await ky.get(`/api/rankings/groups/main`).json();
+  return await api.get(`rankings/groups/main`).json();
 }
 
-export const getGroupInfo = async (id: number): Promise<GroupInfoResponse> => {
-  return await ky.get(`/api/groups/${id}`).json();
+export const getGroupInfo = async (id: string): Promise<GroupInfoResponse> => {
+  return await api.get(`groups/${id}`).json();
 }
 
 export const getGroupList = async (): Promise<GroupListResponse> => {
-  return await ky.get(`/api/groups`).json();
+  return await api.get(`groups`).json();
 }
 
-export const getAvatarUrl = (id:number) : string =>{
-  return `https://api.dicebear.com/9.x/thumbs/svg?seed=${id*10}&scale=80`
+export const getAvatarUrl = (id: number): string => {
+  return `https://api.dicebear.com/9.x/thumbs/svg?seed=${id * 10}&scale=80`;
 }
